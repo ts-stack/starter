ee6b21e44aaa5d8993db29beb937ddda643d181c
KostyaTretyak <ktretiak.in.ua@gmail.com>
2026-06-24 19:52:41 +0300

    Update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,16 @@
 // @ts-check
 
 import eslint from '@eslint/js';
+import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
+export default defineConfig([
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   {
     languageOptions: {
       parserOptions: {
         projectService: true,
-        project: [
-          './tsconfig.json',
-        ]
       },
     },
   },
@@ -21,8 +19,9 @@ export default tseslint.config(
       semi: ['error', 'always'],
       quotes: ['error', 'single', { avoidEscape: true }],
       '@typescript-eslint/no-floating-promises': 'error',
+      '@typescript-eslint/no-misused-promises': 1,
       '@typescript-eslint/no-unused-expressions': 'warn',
-      '@typescript-eslint/no-misused-promises': 0,
+      '@typescript-eslint/consistent-type-imports': 1,
       '@typescript-eslint/no-empty-object-type': 0,
       '@typescript-eslint/no-non-null-assertion': 0,
       '@typescript-eslint/no-empty-function': 0,
@@ -34,7 +33,11 @@ export default tseslint.config(
       '@typescript-eslint/no-non-null-asserted-optional-chain': 0,
       '@typescript-eslint/no-unused-vars': 0,
       '@typescript-eslint/triple-slash-reference': 0,
-      '@typescript-eslint/ban-types': 0,
+      '@typescript-eslint/no-unsafe-function-type': 0,
+      'no-unused-private-class-members': 'warn',
+      'no-useless-assignment': 'warn',
+      'no-restricted-imports': ['error', 'fs'],
+      'prefer-const': 'warn',
       'no-async-promise-executor': 0,
       'no-prototype-builtins': 0,
     },
@@ -47,6 +50,11 @@ export default tseslint.config(
       'node_modules/*',
       'eslint.config.mjs',
       '**/jest.config.ts',
+      '**/vitest.config.ts',
+      '**/jest.setup.js',
+      '**/jest.d.ts',
+      '**/jest.matchers.ts',
+      'packages/openapi/ui/*',
     ],
   },
-);
+]);
